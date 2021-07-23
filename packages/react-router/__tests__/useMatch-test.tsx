5cb64240b6a1095fef0f0882c7f15d4e041b223d
Alireza Safaierad <frontendmonster@gmail.com>
2021-07-23 16:28:16 +0430

    add test case for trailing slash followd by * in useMatch params

--- packages/react-router/__tests__/useMatch-test.tsx
+++ packages/react-router/__tests__/useMatch-test.tsx
@@ -36,6 +36,39 @@ describe("useMatch", () => {
         params: {}
       });
     });
+
+    it("should not include trailing slash in params with /*", () => {
+      let match;
+      function Layout() {
+        match = useMatch("home/*");
+        return null;
+      }
+
+      function Home() {
+        return <h1>Home</h1>;
+      }
+
+      function About() {
+        return <h1>About</h1>;
+      }
+
+      createTestRenderer(
+        <Router initialEntries={["/home/param"]}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/home/param" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
+        </Router>
+      );
+
+      expect(match).toMatchObject({
+        path: "home/*",
+        pathname: "/home",
+        params: { "*": "param" }
+      });
+    });
   });
 
   describe("when the path does not match the current URL", () => {
@@ -43,6 +76,35 @@ describe("useMatch", () => {
       let match;
       function Layout() {
         match = useMatch("about");
+        return null;
+      }
+
+      function Home() {
+        return <h1>Home</h1>;
+      }
+
+      function About() {
+        return <h1>About</h1>;
+      }
+
+      createTestRenderer(
+        <Router initialEntries={["/home"]}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
+        </Router>
+      );
+
+      expect(match).toBe(null);
+    });
+
+    it("returns null for path ends with /*", () => {
+      let match;
+      function Layout() {
+        match = useMatch("h/*");
         return null;
       }
 
